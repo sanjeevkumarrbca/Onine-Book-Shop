6e7c9c0bcd142ad6c6a7221138ca37ba13ab78ef
sanjeevkumarrbca <100609881+sanjeevkumarrbca@users.noreply.github.com>
2023-03-24 10:28:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/main';
+import './components/style.css';
+
+function App() {
+  return (
+    <Main/>
+  );
+}
+
+export default App;
